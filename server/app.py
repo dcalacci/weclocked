295aedd7958ec6fd0f60579f9aadcfafa8be1a39
Dan Calacci <dcalacci@media.mit.edu>
2022-03-23 15:04:08 -0400

    Moves flask server to 'main'

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from project import create_app
+from main import create_app
  
 # Call the Application Factory function to construct a Flask application instance
 # using the standard configuration defined in /instance/flask.cfg
