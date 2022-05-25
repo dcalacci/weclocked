8a40a636838d7d42897da997b37ae5fcf2db8e84
Dan Calacci <dcalacci@media.mit.edu>
2022-05-25 16:44:56 -0400

    Remove old import for 'geo_routes'

--- server/main/weclock_exports/__init__.py
+++ server/main/weclock_exports/__init__.py
@@ -6,7 +6,7 @@ from flask_cors import cross_origin
 # weclock_exports_blueprint = Blueprint('weclock_exports', __name__)
 weclock_exports_ns = Namespace('exports', description='WeClock Export Uploads')
 
-geo_ns= Namespace('geo', description='location data processing',
-                  decorators=[cors.crossdomain(origin="*"), cross_origin()])
+# geo_ns= Namespace('geo', description='location data processing',
+#                   decorators=[cors.crossdomain(origin="*"), cross_origin()])
 
-from . import export_routes, geo_routes
+from . import export_routes
